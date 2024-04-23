39c7358242d23ee73dec2f36fb5d6a101b0419fc
IshanChauhanW3 <ishan.chauhan@w3villa.com>
2024-04-23 17:04:23 +0530

    my changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,67 +1,24 @@
-var stat=-1
-
-// Function to add class to element
-function addClassToElement(elementId, className) {
-    var element = document.getElementById(elementId);
-    console.log(stat)
-    if (element && stat==-1) {
-       element.classList.add(className);
-        
-        stat=1
-        console.log(stat)
-    }
-    else if(element && stat==1){
-        element.classList.remove(className);
-        stat= -1;
-        console.log(stat)        
-    }
-     else {
-      console.error("Element with ID '" + elementId + "' not found.");
-      return 
-    }
-  }
-  
-  // Add event listener to the anchor tag
-  
-  var addClass = document.getElementById('dropdown-menubar');
-  if (addClass) {
-    addClass.addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent default anchor behavior
-    addClassToElement('menubar', 'show');
-    console.log("stat"+stat)
-    });
-  } else {
-    console.error("Anchor tag with ID 'addClassLink' not found.");
-  }
-
-  var addClass = document.getElementById('login-logout');
-  if (addClass) {
-    addClass.addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent default anchor behavior
-    addClassToElement('navbar-dropdown', 'navbar-dropdown');
-    console.log("stat"+stat)
-    });
-  } else {
-    console.error("Anchor tag with ID 'addClassLink' not found.");
-  }
-
-  var addClass = document.getElementById('nav-dropdown');
-  if (addClass) {
-    addClass.addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent default anchor behavior
-    addClassToElement('navbaroptions-mobile', 'navbaroptions-mobile');
-    console.log("stat"+stat)
-    });
-  } else {
-    console.error("Anchor tag with ID 'addClassLink' not found.");
-  }
-
-  var addClass=document.getElementById('mobile-search-button')
-  if(addClass){
-addClass.addEventListener('click',(e)=>{
+var dropdownMenubar=document.getElementById("dropdown-menubar")
+dropdownMenubar.onclick=function(e){
+  console.log(1)
+  var menubar=document.getElementById("menubar");
   e.preventDefault();
-  addClassToElement('search-mobile','search-mobile');
-});}
-else{
-  console.error("Anchor tag with ID 'addClassLink' not found.");
+  menubar.classList.toggle("show")
+  console.log(1)
+}
+
+document.getElementById("mobile-search-button").onclick=function(e){
+  e.preventDefault();
+  var searchMobile=document.getElementById("search-mobile")
+  searchMobile.classList.toggle("search-mobile")
+}
+document.getElementById("login-logout").onclick=function(e){
+  e.preventDefault();
+  var navbarDropdown=document.getElementById("navbar-dropdown")
+  navbarDropdown.classList.toggle("navbar-dropdown")
+}
+document.getElementById("nav-dropdown").onclick=function(e){
+  e.preventDefault();
+  var navbaroptionsMobile=document.getElementById("navbaroptions-mobile")
+  navbaroptionsMobile.classList.toggle("navbaroptions-mobile")
 }
